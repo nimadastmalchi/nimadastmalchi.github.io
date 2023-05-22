ac7d1a3134913f9715ffe9504c68f17e46f654ff
Nima Amir Dastmalchi <nima.dastmalchi@gmail.com>
2023-05-21 23:50:07 -0700

    Updated projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
-
-import Cell from '../components/Cell';
-import data from '../data/projects';
+import dayjs from 'dayjs';
 
 const Projects = () => (
   <Main
     title="Projects"
-    description="Learn about Michael D'Angelo's projects."
+    description="Learn about Nima Dastmalchi's projects."
   >
     <article className="post" id="projects">
       <header>
@@ -18,12 +16,41 @@ const Projects = () => (
           <p>Some of my notable projects</p>
         </div>
       </header>
-      {data.map((project) => (
-        <Cell
-          data={project}
-          key={project.title}
-        />
-      ))}
+      <div className="cell-container">
+        <article className="mini-post">
+          <header>
+            <h3><a href={"https://github.com/nimadastmalchi/csp-ray-tracer"}>{"Concentrated Solar Power (CSP) Simulation"}</a></h3>
+            <time className="published">{dayjs("2020-09-01").format('MMMM, YYYY')}</time>
+          </header>
+          <a href={"https://github.com/nimadastmalchi/csp-ray-tracer"} className="image">
+            <img src={`${process.env.PUBLIC_URL}${"/images/projects/csp-system.jpeg"}`} alt={"Ray tracing simulation"} />
+          </a>
+          <a href={"https://github.com/nimadastmalchi/csp-ray-tracer"} className="image">
+            <img src={`${process.env.PUBLIC_URL}${"/images/projects/csp.jpg"}`} alt={"Ray tracing simulation"} />
+          </a>
+          <a href={"https://www.honorstransfercouncil.org/_files/ugd/0c62f0_f0806b39d13744dba1d8cf5e34e085b9.pdf"} className="image">
+            <img src={`${process.env.PUBLIC_URL}${"/images/projects/poster.png"}`} alt={"Ray tracing simulation"} />
+          </a>
+          <div className="description">
+            <p>{"Concentrated Solar Power (CSP) systems generate solar power by using mirrors or lenses to concentrated a receiver"}</p>
+            <p>{"A C++ simulator program utilizes ray-tracing and various concepts from physics (reflection, refraction, snell's law, newton's law of cooling, heat capacity, etc.) to calculate the temperature and power output of a given CSP system."}</p>
+            <p>{"Published a research paper on the 2020 edition of UC Berkeley's 'Think You?!' academic journal."}</p>
+          </div>
+        </article>
+        <article className="mini-post">
+          <header>
+            <h3><a href={"https://github.com/nimadastmalchi/scramboard-app"}>{"Scramboard"}</a></h3>
+            <time className="published">{dayjs("2022-03-01").format('MMMM, YYYY')}</time>
+          </header>
+          <a href={"https://github.com/nimadastmalchi/scramboard-app"} className="image">
+            <img src={`${process.env.PUBLIC_URL}${"/images/projects/scramboard.jpg"}`} alt={"Ray tracing simulation"} />
+          </a>
+          <div className="description">
+            <p>{"Inspired by the Reddit Place project, Scramboard provides a collective pixel-board that can be edited in real-time by users logged into the site. Users can edit individual pixels and collectively create pixel art images."}</p>
+            <p>{"Used React, Node, Express, Python."}</p>
+          </div>
+        </article>
+      </div>
     </article>
   </Main>
 );
